ec836190311d7e3734bc71d971a842b7756e7ae0
JoannaGoj <joannagojan@gmail.com>
2022-08-23 00:24:58 +0200

    saving

--- accounts/views.py
+++ accounts/views.py
@@ -11,26 +11,19 @@ from django.contrib.auth import authenticate, login
 class Login(View):
     def get(self, request):
         form = LoginForm
-        print('0')
         return render(request, 'form_template.html', {'form':form})
 
     def post(self, request):
         form = LoginForm(request.POST)
-        print('1')
         if form.is_valid():
-            print('2')
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = authenticate(request,username=username, password=password)
-            print('3')
             if user is not None:
-                print('4')
                 login(request, user)
                 url = request.GET.get('next', reverse('manage_tags'))
-                print('5')
                 return redirect(url)
         else:
-            print('6')
             form = LoginForm
         return render(request, 'form_template.html', {'form': form, 'message':'nope'})
 
